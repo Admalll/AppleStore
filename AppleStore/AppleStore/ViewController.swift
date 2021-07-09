697d684a48bddbf59d8d25f87d6797c9ad6df98c
Admalll <F1zzer1997195@gmail.com>
2021-07-09 10:28:06 +0300

    Init clean project

---
+++ AppleStore/AppleStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleStore
+//
+//  Created by Vlad on 09.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
