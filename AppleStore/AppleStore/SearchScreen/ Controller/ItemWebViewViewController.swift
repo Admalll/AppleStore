e8845bdedb0b173df72ca8df8b88708010279bd7
Admalll <F1zzer1997195@gmail.com>
2021-07-13 15:59:52 +0300

    Исправил замечания

--- AppleStore/AppleStore/SearchScreen/ Controller/ItemWebViewViewController.swift
+++ AppleStore/AppleStore/SearchScreen/ Controller/ItemWebViewViewController.swift
@@ -13,6 +13,10 @@ enum Const {
     static let iphone = "https://www.apple.com/ru/iphone/"
     static let caseBrown = "https://www.apple.com/ru/shop/product/MRQM2ZM/A/%D0%BA%D0%BE%D0%B6%D0%B0%D0%BD%D1%8B%D0%B9-%D1%87%D0%B5%D1%85%D0%BE%D0%BB-%D0%B4%D0%BB%D1%8F-13%E2%80%91%D0%B4%D1%8E%D0%B9%D0%BC%D0%BE%D0%B2%D1%8B%D1%85-macbook-air-%D0%B8-macbook-pro-%D0%B7%D0%BE%D0%BB%D0%BE%D1%82%D0%B8%D1%81%D1%82%D0%BE%E2%80%91%D0%BA%D0%BE%D1%80%D0%B8%D1%87%D0%BD%D0%B5%D0%B2%D1%8B%D0%B9-%D1%86%D0%B2%D0%B5%D1%82?fnode=0d6062b669ad91588d35d0aabb4627d1b0062b9755451111cdb2f226be3f9cad8d9af0b663de62dcdc9b09e8e7ed9559537052afd7f5ecb12f5cde293ff03c22f633d51e25597a15d7c850150b34735f44bfadea22ee3af4350f2c395993f72855313d85d98a7f74595a87e66ffdf7ee"
     static let belt = "https://www.apple.com/ru/shop/watch/bands/%D1%81%D0%BF%D0%BE%D1%80%D1%82%D0%B8%D0%B2%D0%BD%D1%8B%D0%B9-%D1%80%D0%B5%D0%BC%D0%B5%D1%88%D0%BE%D0%BA"
+    static let caseItemName = "Чехол Incase Flat для MacBook pro 16 дюймов"
+    static let iphoneItemName = "iPhone 12 Pro Max 256 GB"
+    static let caseBownItemName = "Кожаный чехол для MacBook Pro 16 дюймов, золотой"
+    static let beltItemName = "Спортивный ремешок Black Unity (для к..."
 }
 
 final class ItemWebViewViewController: UIViewController {
@@ -45,17 +49,16 @@ final class ItemWebViewViewController: UIViewController {
     }
 
     private func setupWebView() {
-        webView.uiDelegate = self
         webView.navigationDelegate = self
 
         switch itemName {
-        case "Чехол Incase Flat для MacBook pro 16 дюймов":
+        case Const.caseItemName:
             setupUrl(string: Const.caseGray)
-        case "Спортивный ремешок Black Unity (для к...":
+        case Const.beltItemName:
             setupUrl(string: Const.belt)
-        case "Кожаный чехол для MacBook Pro 16 дюймов, золотой":
+        case Const.caseBownItemName:
             setupUrl(string: Const.caseBrown)
-        case "iPhone 12 Pro Max 256 GB":
+        case Const.iphoneItemName:
             setupUrl(string: Const.iphone)
         default:
             break
@@ -100,16 +103,8 @@ final class ItemWebViewViewController: UIViewController {
     }
 
     private func setupBarButtonNavigationItems() {
-        if webView.canGoBack {
-            goBackItem.isEnabled = true
-        } else {
-            goBackItem.isEnabled = false
-        }
-        if webView.canGoForward {
-            goForwardItem.isEnabled = true
-        } else {
-            goForwardItem.isEnabled = false
-        }
+        goBackItem.isEnabled = webView.canGoBack
+        goForwardItem.isEnabled = webView.canGoForward
     }
 
     @objc private func goBack() {
@@ -131,10 +126,6 @@ final class ItemWebViewViewController: UIViewController {
         }
     }
 }
-
-//MARK: - WKUIDelegate
-
-extension ItemWebViewViewController: WKUIDelegate { }
 
 //MARK: - WKNavigationDelegate
 
